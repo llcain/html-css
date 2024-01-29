206e583190c4aaae11646a1087fa7f14630f1409
llcain <llcain2012@gmail.com>
2024-01-28 19:26:40 -0600

    Created an object from a JSON string

--- about-script.js
+++ about-script.js
@@ -13,3 +13,8 @@ const element = document.getElementById('div1');
 const child = document.getElementById("p1");
 element.insertBefore(para, child);
 
+// creating an object from a json string
+const txt = '{"name":"John", "age": 30, "city":"New York"}'
+const obj = JSON.parse(txt);
+document.getElementById('demo1').innerHTML = obj.name + ", " + obj.age;
+
