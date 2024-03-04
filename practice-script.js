b788f15377ce4d3b12d92079069e8daf4cdd1c0e
llcain <llcain2012@gmail.com>
2024-03-04 13:10:07 -0600

    Add element function

--- practice-script.js
+++ practice-script.js
@@ -11,3 +11,18 @@ const person = {
 }
 
 document.getElementById("demo").innerHTML = person.eyeColor;
+
+function addElement() {
+
+    // create element
+    const li = document.createElement('li');
+
+    // create text node
+    const textNode = document.createTextNode("water");
+
+    // append the text node to the li node:
+    li.appendChild(textNode);
+
+    // append the li node to the list
+    document.getElementById("myList").appendChild(li);
+}
